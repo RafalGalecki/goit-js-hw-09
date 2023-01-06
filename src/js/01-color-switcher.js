830df2217c770422d16adef5d72acc1156459f8a
Rafał Gałecki <darthunter@gmail.com>
2023-01-06 18:12:46 +0100

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let colorChange = null;
+
+btnStart.addEventListener('click', () => {
+  colorChange = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(colorChange);
+  btnStart.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
