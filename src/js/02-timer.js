f30dfcfdc8abd21029c8e9171779ceacbfc934e5
Rafał Gałecki <darthunter@gmail.com>
2023-01-08 13:10:24 +0100

    timer works

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,6 +5,12 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const btnStart = document.querySelector('[data-start]');
+
+const dayTimer = document.querySelector('[data-days');
+const hourTimer = document.querySelector('[data-hours');
+const minuteTimer = document.querySelector('[data-minutes');
+const secondTimer = document.querySelector('[data-seconds');
+
 btnStart.disabled = true;
 
 const options = {
@@ -13,14 +19,22 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    let timeDifference = selectedDates[0].getTime() - new Date().getTime();
+    let pickedDate = selectedDates[0].getTime();
+    let timeDifference = pickedDate - new Date().getTime();
     if (timeDifference <= 0) {
       alert('Please choose a date in the future.');
       btnStart.disabled = true;
     } else {
       btnStart.disabled = false;
+
+      btnStart.addEventListener('click', () => {
+        countdownTimer(pickedDate);
+        btnStart.disabled = true;
+      });
+
       console.log(1, selectedDates[0]);
       console.log(convertMs(timeDifference));
+      //dayTimer.textContent = '11';
     }
   },
 };
@@ -45,4 +59,23 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(val) {
+  return String(val).padStart(2, '0');
+}
+//console.log('pickedDate', pickedDate);
+function countdownTimer(pickedDate) {
+  const countdownUnit = setInterval(() => {
+    ms = pickedDate - new Date().getTime();
+    const dropTime = convertMs(ms);
+
+    dayTimer.textContent = addLeadingZero(dropTime.days);
+    hourTimer.textContent = addLeadingZero(dropTime.hours);
+    minuteTimer.textContent = addLeadingZero(dropTime.minutes);
+    secondTimer.textContent = addLeadingZero(dropTime.seconds);
+    if (ms < 1000) {
+      clearInterval(countdownUnit);
+    }
+  }, 1000);
 }
